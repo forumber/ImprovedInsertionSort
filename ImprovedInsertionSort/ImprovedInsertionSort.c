b86fc4d743d2a8f43078f49dec1214dfc40e44a0
forumber <kidobeko@gmail.com>
2019-12-01 19:17:45 +0200

    Loopify operations

--- ImprovedInsertionSort/ImprovedInsertionSort.c
+++ ImprovedInsertionSort/ImprovedInsertionSort.c
@@ -6,6 +6,7 @@
 #pragma warning(disable:4996) // remove security warnings on VS
 #pragma warning(disable:6031) // remove return is ignored warnings on VS
 #pragma warning(disable:6011) // remove dereference warnings on VS
+#pragma warning(disable:4018) // remove operator signed-unsigned mismatch warnings on VS
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -101,7 +102,7 @@ struct listDef improvedInsertionSort(int numbers[], unsigned int numbersCount)
 	clock_t t = clock(); // start time
 
 	printf("Creating and filling 10 list\n");
-	unsigned int i;
+	unsigned int i, j;
 
 	int min = numbers[0];
 	int max = numbers[0];
@@ -119,107 +120,46 @@ struct listDef improvedInsertionSort(int numbers[], unsigned int numbersCount)
 	if (range <= 10)
 		range = 11;
 
-	struct listDef list1;
-	struct listDef list2;
-	struct listDef list3;
-	struct listDef list4;
-	struct listDef list5;
-	struct listDef list6;
-	struct listDef list7;
-	struct listDef list8;
-	struct listDef list9;
-	struct listDef list10;
+	struct listDef lists[10];
 
-	list1.elementCount = 0;
-	list2.elementCount = 0;
-	list3.elementCount = 0;
-	list4.elementCount = 0;
-	list5.elementCount = 0;
-	list6.elementCount = 0;
-	list7.elementCount = 0;
-	list8.elementCount = 0;
-	list9.elementCount = 0;
-	list10.elementCount = 0;
+	for (i = 0; i < 10; i++)
+		lists[i].elementCount = 0;
 
-	list1.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list2.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list3.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list4.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list5.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list6.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list7.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list8.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list9.theList = (int*)malloc((numbersCount) * sizeof(int));
-	list10.theList = (int*)malloc((numbersCount) * sizeof(int));
+	for (i = 0; i < 10; i++)
+		lists[i].theList = (int*)malloc((numbersCount) * sizeof(int));
 
 	for (i = 0; i < numbersCount; i++)
 	{
 		if ((numbers[i] >= min) && (numbers[i] < min + (range / 10)))
 		{
-			list1.theList[list1.elementCount] = numbers[i];
-			list1.elementCount++;
+			lists[0].theList[lists[0].elementCount] = numbers[i];
+			lists[0].elementCount++;
 		}
-		else if ((numbers[i] >= min + (range / 10)) && (numbers[i] < min + ((2 * range) / 10)))
+
+		for (j = 1; j < 9; j++)
 		{
-			list2.theList[list2.elementCount] = numbers[i];
-			list2.elementCount++;
+			if ((numbers[i] >= min + ((j * range) / 10)) && (numbers[i] < min + (((j + 1) * range) / 10)))
+			{
+				lists[j].theList[lists[j].elementCount] = numbers[i];
+				lists[j].elementCount++;
+				break;
+			}
 		}
-		else if ((numbers[i] >= min + ((2 * range) / 10)) && (numbers[i] < min + ((3 * range) / 10)))
+		
+		if ((numbers[i] >= min + ((9 * range) / 10)) && (numbers[i] <= max))
 		{
-			list3.theList[list3.elementCount] = numbers[i];
-			list3.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((3 * range) / 10)) && (numbers[i] < min + ((4 * range) / 10)))
-		{
-			list4.theList[list4.elementCount] = numbers[i];
-			list4.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((4 * range) / 10)) && (numbers[i] < min + ((5 * range) / 10)))
-		{
-			list5.theList[list5.elementCount] = numbers[i];
-			list5.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((5 * range) / 10)) && (numbers[i] < min + ((6 * range) / 10)))
-		{
-			list6.theList[list6.elementCount] = numbers[i];
-			list6.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((6 * range) / 10)) && (numbers[i] < min + ((7 * range) / 10)))
-		{
-			list7.theList[list7.elementCount] = numbers[i];
-			list7.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((7 * range) / 10)) && (numbers[i] < min + ((8 * range) / 10)))
-		{
-			list8.theList[list8.elementCount] = numbers[i];
-			list8.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((8 * range) / 10)) && (numbers[i] < min + ((9 * range) / 10)))
-		{
-			list9.theList[list9.elementCount] = numbers[i];
-			list9.elementCount++;
-		}
-		else if ((numbers[i] >= min + ((9 * range) / 10)) && (numbers[i] <= max))
-		{
-			list10.theList[list10.elementCount] = numbers[i];
-			list10.elementCount++;
+			lists[9].theList[lists[9].elementCount] = numbers[i];
+			lists[9].elementCount++;
 		}
 	}
 
 	pthread_t threads[10];
 
-	printf("Begin of regular insertion sort to 10 list\n\n");
+	printf("Begin regular insertion sort to 10 list\n\n");
 	// Creating and starting threads
-	pthread_create(&threads[0], NULL, insertionSort, (void*)&list1);
-	pthread_create(&threads[1], NULL, insertionSort, (void*)&list2);
-	pthread_create(&threads[2], NULL, insertionSort, (void*)&list3);
-	pthread_create(&threads[3], NULL, insertionSort, (void*)&list4);
-	pthread_create(&threads[4], NULL, insertionSort, (void*)&list5);
-	pthread_create(&threads[5], NULL, insertionSort, (void*)&list6);
-	pthread_create(&threads[6], NULL, insertionSort, (void*)&list7);
-	pthread_create(&threads[7], NULL, insertionSort, (void*)&list8);
-	pthread_create(&threads[8], NULL, insertionSort, (void*)&list9);
-	pthread_create(&threads[9], NULL, insertionSort, (void*)&list10);
+
+	for (i = 0; i < 10; i++)
+		pthread_create(&threads[i], NULL, insertionSort, (void*)&lists[i]);
 
 	// Waiting for threads to complete
 	for (i = 0; i < 10; i++)
@@ -227,36 +167,27 @@ struct listDef improvedInsertionSort(int numbers[], unsigned int numbersCount)
 
 	printf("\nRegular insertion sort to 10 list has been completed\n");
 
-	printf("Begin of merging 10 list into 5 sublist\n\n");
-	struct listDef subList1;
-	struct listDef subList2;
-	struct listDef subList3;
-	struct listDef subList4;
-	struct listDef subList5;
+	printf("Begin merging 10 list into 5 sublist\n\n");
+	struct listDef subLists[5];
 
-	subList1.theList = merge2List(list1.theList, list2.theList, list1.elementCount, list2.elementCount, &subList1.elementCount);
-	subList2.theList = merge2List(list3.theList, list4.theList, list3.elementCount, list4.elementCount, &subList2.elementCount);
-	subList3.theList = merge2List(list5.theList, list6.theList, list5.elementCount, list6.elementCount, &subList3.elementCount);
-	subList4.theList = merge2List(list7.theList, list8.theList, list7.elementCount, list8.elementCount, &subList4.elementCount);
-	subList5.theList = merge2List(list9.theList, list10.theList, list9.elementCount, list10.elementCount, &subList5.elementCount);
+	for (i = 0, j = 0; i < 5; i++, j = j + 2)
+		subLists[i].theList = merge2List(lists[j].theList, lists[j + 1].theList, lists[j].elementCount, lists[j + 1].elementCount, &subLists[i].elementCount);
+
 	printf("\nMerging 10 list into 5 sublist has been completed\n");
 
-	printf("Begin of merging 5 list into 3 sublist\n\n");
-	struct listDef subSubList1;
-	struct listDef subSubList2;
+	printf("Begin merging 5 list into 3 sublist\n\n");
+	struct listDef subSubLists[3];
 
-	subSubList1.theList = merge2List(subList1.theList, subList2.theList, subList1.elementCount, subList2.elementCount, &subSubList1.elementCount);
-	subSubList2.theList = merge2List(subList3.theList, subList4.theList, subList3.elementCount, subList4.elementCount, &subSubList2.elementCount);
+	for (i = 0, j = 0; i < 2; i++, j = j + 2)
+		subSubLists[i].theList = merge2List(subLists[j].theList, subLists[j + 1].theList, subLists[j].elementCount, subLists[j + 1].elementCount, &subSubLists[i].elementCount);
 
-	struct listDef subSubList3;
+	subSubLists[2].theList = merge2List(subSubLists[0].theList, subSubLists[1].theList, subSubLists[0].elementCount, subSubLists[1].elementCount, &subSubLists[2].elementCount);
 
-	subSubList3.theList = merge2List(subSubList1.theList, subSubList2.theList, subSubList1.elementCount, subSubList2.elementCount, &subSubList3.elementCount);
 	printf("\nMerging 5 list into 3 sublist has been completed\n");
-
-	printf("Begin of last merge operation\n\n");
+	printf("Begin last merge operation\n\n");
 	struct listDef sortedList;
 
-	sortedList.theList = merge2List(subList5.theList, subSubList3.theList, subList5.elementCount, subSubList3.elementCount, &sortedList.elementCount);
+	sortedList.theList = merge2List(subLists[4].theList, subSubLists[2].theList, subLists[4].elementCount, subSubLists[2].elementCount, &sortedList.elementCount);
 	printf("\nLast merge operation has been completed\n\n");
 
 	t = clock() - t;
